e72c3b7a8fb205e50a78176adc18d99e11e5dc51
Sky201135 <kazisakiya11@outlook.com>
2025-10-17 22:52:04 +0600

    smth

---
+++ Homework.py
@@ -0,0 +1,7 @@
+name = input("Enter the person you are congratulating's name: ")
+
+reason = input("Enter the reason for congratulating: ")
+
+user = input("Enter your name: ")
+
+print(name + ", congratulations on your " + reason + " achievement!" + " hope you are enjoying your success! Love from, " + user + "!!!")
