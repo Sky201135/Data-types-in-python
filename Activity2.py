96a2ee9bfc550f87517820fb927ba59989ea3454
Sky201135 <kazisakiya11@outlook.com>
2025-10-17 22:34:11 +0600

    smth

---
+++ Activity2.py
@@ -0,0 +1,21 @@
+name = "sophie"
+age = 14
+is_student = True
+weight = 50.5
+
+print("Name:", name)
+print("Data type of Name:", type(name))
+print("Age:", age)
+print("Data type of Age:", type(age))
+print("Is Student:", is_student)
+print("Data type of Is Student:", type(is_student))
+print("Weight:", weight)
+print("Data type of Weight:", type(weight))
+
+print("\nAfter Type Casting...")
+age = str(age)
+print(age)
+print("Data type of age:", type(age))
+weight = int(weight)
+print(weight)
+print("Data type of weight:", type(weight))
